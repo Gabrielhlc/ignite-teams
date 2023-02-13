8ba9dae90cb1e99777f5312183e4a823095d1674
Gabrielhlc <gabrielhenlc@gmail.com>
2023-02-13 20:20:05 -0300

    add components

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,39 @@
+import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
+
+import { Container } from './styles'
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
+
+export function Groups() {
+    const [groups, setGroups] = useState<string[]>([])
+
+
+    return (
+        <Container>
+            <Header />
+            <Highlight
+                title='Turmas'
+                subtitle='jogue com a sua turma'
+            />
+
+            <FlatList
+                data={groups}
+                keyExtractor={item => item}
+                renderItem={({ item }) => (
+                    <GroupCard title={item}
+                    />
+                )}
+                contentContainerStyle={{ flex: 1 }}
+                ListEmptyComponent={() => (
+                    <ListEmpty message='Que tal cadastrar a primeira turma?' />
+                )}
+            />
+
+            <Button title='Criar nova turma' type='SUCCESS' />
+        </Container>
+    );
+}
